467ce0f4b8e9bcaf0bf413c152a9042feec2fe8e
sayantancodex <sayantanpatra68@gmail.com>
2026-05-06 00:12:39 +0530

    Bump version to 0.2.5 and re-trigger release.

--- dfxpy/__init__.py
+++ dfxpy/__init__.py
@@ -7,7 +7,7 @@ from .feature_selection import suggest_features
 from .cleaning import clean_column_names, handle_missing, infer_types
 from .io import load, read_csv, read_excel, DataFrame
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 __all__ = [
     "auto",
     "eda",
